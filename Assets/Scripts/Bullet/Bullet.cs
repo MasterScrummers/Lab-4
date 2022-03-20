457e041eac8390bad0840bc22e2097c74efa07db
Ayush <kushal.a.bilash@student.uts.edu.au>
2022-03-21 09:53:06 +1100

    Bullet 3D Perspective Added
    "As a player, I want the bullet to get smaller as it gets closer to the centre of the screen so that I can tell it's further away"
    
    Bullet changes in size as it gets closer to center of screen to mimic 3D perspective

--- Assets/Scripts/Bullet/Bullet.cs
+++ Assets/Scripts/Bullet/Bullet.cs
@@ -4,19 +4,24 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public float speed = 2; //bullet speed
+    public float speed = 1.5f; //bullet speed
     private Rigidbody2D rb;
+    private Vector3 originalScale;
+    private Vector3 targetScale;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        originalScale = transform.localScale;
+        targetScale = new Vector3(0.01f, 0.02f, 0.01f); //y-axis slightly bigger to mimic depth distoriton
     }
 
     // Update is called once per frame
     void Update()
     {
         rb.velocity = transform.up * speed; //bullet being fired along y-axis of player
+        transform.localScale = Vector3.Lerp(transform.localScale, targetScale, Time.deltaTime * speed); //Bullet getting smaller as it gets further away
 
         //This is just a dummy for now, will be removed once we figure out perspective
         if (transform.position.y >= -0.2 && transform.position.y <= 0.2 && transform.position.x >= -0.2 && transform.position.x <= 0.2)
