6a8c4c39dd68db78e7ed9ee12ba42cedd0d81b27
NovGS <70025949+NovGS@users.noreply.github.com>
2022-03-24 01:17:11 +1100

    Enemy Spawner

--- Assets/Scripts/Enemies/EnemySpawner.cs
+++ Assets/Scripts/Enemies/EnemySpawner.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,6 +11,7 @@ public class EnemySpawner : MonoBehaviour
     private MainGameStartUp startUpRef; //Reference of the main game start up.
 
     private float spawnTimer;
+    public float bossCounter = 180.0f;
 
     void Start()
     {
@@ -29,6 +31,11 @@ public class EnemySpawner : MonoBehaviour
             spawnTimer = spawningIntervals;
             SpawnWave();
         }
+
+        if (startUpRef.stage == 3)
+        {
+            bossCounter -= Time.deltaTime;
+        }
     }
 
     private void SpawnWave()
@@ -36,16 +43,65 @@ public class EnemySpawner : MonoBehaviour
         switch (startUpRef.stage)
         {
             case 1:
-                SpawnEnemy(2);
+                StartCoroutine(SpawnControl(1));
                 return;
             case 2:
+                StartCoroutine(SpawnControl(2));
                 return;
             case 3:
-
+                StartCoroutine(SpawnControl(3));
                 return;
         }
     }
 
+    private IEnumerator SpawnControl(int stage)
+    {
+        if (stage == 1)
+        {
+            for (int i = 0; i < Random.Range(1, 4); i++)
+            {
+                SpawnEnemy(0);
+            }
+            yield return new WaitForSecondsRealtime(Random.Range(2, 5));
+            SpawnEnemy(3);
+        }
+
+        if (stage == 2)
+        {
+            for (int i = 0; i < Random.Range(1, 4); i++)
+            {
+                SpawnEnemy(Random.Range(3, 5));
+            }
+            yield return new WaitForSecondsRealtime(Random.Range(1, 4));
+            for (int i = 0; i < Random.Range(3, 5) ; i++)
+            {
+                SpawnEnemy(0);
+            }
+        }
+
+        if (stage == 3)
+        {
+            if (bossCounter >= 0)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    SpawnEnemy(0);
+                }
+
+                for (int i = 0; i < 2; i++)
+                {
+                    SpawnEnemy(3);
+                }
+            }
+
+            if (bossCounter <= 0)
+            {
+                Debug.Log("Boss Spawned");
+                SpawnEnemy(5);
+            }
+        }
+            
+    }
     private void SpawnEnemy(int index)
     {
         string enemyName = typesOfEnemies[index].name;
